0bdd77cb75990fb533be73483cd5eeae60a3412d
DenilDenilson <jordanpreseleccion@gmail.com>
2022-03-15 10:51:04 -0500

    Solucionado el tema del borde negro, era el outline del focus que viene por defecto, mal ahí tailwind, agregado el main.js -> background button

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const textAlert = document.querySelector('#text_alert');
+const iconAlert = document.querySelector('#icon_alert');
+const submitButton = document.querySelector('#submit_button');
+const inputEmail = document.querySelector('#input_email');
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!regexEmail.test(inputEmail.value))
+    {
+        iconAlert.classList.remove('invisible');
+        iconAlert.classList.add('visible');
+        textAlert.classList.remove('invisible');
+        textAlert.classList.add('visible');
+        console.log('NoSexo');
+    }
+    if (regexEmail.test(inputEmail.value))
+    {
+        iconAlert.classList.remove('visible')
+        iconAlert.classList.add('invisible');
+        textAlert.classList.remove('visible');
+        textAlert.classList.add('invisible');
+        console.log('Sexo');
+    }
+});

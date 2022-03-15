93b7c5293165771a03113a64d6cbf2bccc57a877
DenilDenilson <jordanpreseleccion@gmail.com>
2022-03-14 23:30:23 -0500

    Eliminé y agregue cosas extras específicas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './src/**/*.{html, js}',
-    './index.html',
+    //'./index.html',
+    './*.{html, js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Josefina_sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
